350be76a4fb593604434b7dd867ee61c4836315a
Mike Decaro <mdecaro@imo-online.com>
2022-05-16 16:56:24 -0500

    Move loop outside function

--- medium_dict_vs_list_of_maps/access_list_maps.py
+++ medium_dict_vs_list_of_maps/access_list_maps.py
@@ -4,33 +4,33 @@ with open('bb_data_list_maps.json', 'r') as f:
 
 
 # Find the total OPS for first basemen, by team they played for
-def populate_position_ops(pos):
-    # print(f"{i['id']}: {i['first_name']} {i['last_name']}")
+def populate_position_ops(player, pos):
+    '''
+    Given a player and a position, return a dictionary 
+    that includes their OPS for every team at that position
+    '''
+    # Initialize an empty dictionary for the player
     player_dict = {}
-    for team in i['teams']:
+    for team in player['teams']:
+        # Since we calculate stats within a team, reset OPS for each new team
         ops_sum = 0
         plate_app = 0
         for year in team['years']:
             for position in year['positions']:
+                # Only modify the OPS given the position matches out input
                 if position['position'] == pos:
                     ops_sum = ops_sum + position['ops']*position['pa']
                     plate_app = plate_app + position['pa']
+        # Only create an entry in the player dict if there were
+        # PAs for the team at the position in question
         if plate_app > 0:
             ops_total = ops_sum / plate_app
             player_dict[team['team']] = ops_total
+    # Only return the player dict if there if the player ever appeared at the position
     if player_dict:
-        return {f"{i['first_name']} {i['last_name']}": player_dict}
-
-'''
-{
-    "Jeff Liefer": {
-        "Chicago White Sox": ####,
-        "Montreal Expos": ####
-    }
-}
-'''
+        return {f"{player['first_name']} {player['last_name']}": player_dict}
 
 if __name__ == '__main__':
     first_basemen = {}
-    for i in json_data:
-        print(populate_position_ops('1b'))
+    for p in json_data:
+        print(populate_position_ops(p, '1b'))
